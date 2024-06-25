f4e41cc54b0930ce6b19e34bb3a5df32226b4f27
Biyu-16 <155044746+Biyu-16@users.noreply.github.com>
2024-06-25 19:32:37 +0300

    Update register.js

--- src/register.js
+++ src/register.js
@@ -14,7 +14,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = axios.post("http://localhost:3001/register", {
+      const response = axios.post("https://back-end-8dc5.onrender.com/register", {
         email,
         password,
       });
